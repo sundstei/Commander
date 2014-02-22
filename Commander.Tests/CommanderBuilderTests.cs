e647a08884a79db48690f6ae2faa77c720e3e970
Svein Erik <sundstei@gmail.com>
2014-02-22 13:45:41 +0100

    Created test project and main project.

---
+++ Commander.Tests/CommanderBuilderTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Commander.Tests
+{
+    [TestClass]
+    public class CommanderBuilderTests
+    {
+        [TestMethod]
+        public void Should_parse_args_to_command_structure()
+        {
+        }
+    }
+}
