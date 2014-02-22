0e54eb444f803330b5b2392b397d9444c2c862d3
Svein Erik <sundstei@gmail.com>
2014-02-22 13:45:58 +0100

    Created commandbuilder for args.

---
+++ Commander/CommanderBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Commander
+{
+    /// <summary>
+    /// Utility class for building a logical structure from command line arguments.
+    /// </summary>
+    public class CommanderBuilder
+    {
+        /// <summary>
+        /// Build a command structure from command line arguments.
+        /// </summary>
+        /// <param name="commandLineArguments"></param>
+        public CommanderBuilder(IEnumerable<string> commandLineArguments)
+        {
+            ParseArguments(commandLineArguments);
+        }
+
+        private void ParseArguments(IEnumerable<string> commandLineArguments)
+        {
+            var arguments = new Dictionary<string, string>();
+
+            foreach (var argument in commandLineArguments)
+            {
+                if (argument.Contains(":"))
+                {
+                    var args = argument.Split(':');
+                    if (args.Length < 2)
+                        throw new Exception("Error reading arguments.");
+
+                    arguments.Add(args[0], args[1]);
+                }
+                else
+                    arguments["Default"] = argument;
+            }
+        }
+    }
+}
